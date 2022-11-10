3eb76d608321c39f054a9396f257f09dadcad519
worm704 <109051047@live.asia.edu.tw>
2022-11-10 10:50:20 +0800

    完成品

---
+++ Unity2DGame/Assets/Scripts/DamageSystem.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+namespace Nick
+{
+    /// <summary>
+    /// 傷害系統
+    /// </summary>
+
+    public class DamageSystem : MonoBehaviour
+    {
+        [SerializeField, Header("爆炸預製物")]
+        private GameObject prefabExplosion;
+        [SerializeField, Header("碰到會爆炸的名稱")]
+        private string nameTarget;
+    }
+}
